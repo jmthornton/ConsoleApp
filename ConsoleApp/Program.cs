bf8ede8eeb199ea4822b2692627e4501929619aa
John Thornton <john.thornton@forensic-pathways.com>
2014-11-28 15:48:33 +0000

    Added numerics package and example code.

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MathNet.Numerics.LinearAlgebra;
+using MathNet.Numerics.LinearAlgebra.Double;
 
 namespace ConsoleApp
 {
@@ -10,8 +12,16 @@ namespace ConsoleApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-            Console.Read();
+            var m = Matrix<double>.Build.Random(6, 5);
+            var n = Matrix<double>.Build.Random(5, 3);
+
+            var v = m * n;
+
+            Console.WriteLine(v);
+            Console.WriteLine(v.Transpose());
+
+            Console.WriteLine("Done.  Press a key...");
+            Console.ReadKey();
         }
     }
 }
